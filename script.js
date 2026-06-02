e96106c292c39ab55fa415cc2b72a01017e95cfc
Matheus <matheus.mansini123@gmail.com>
2026-06-02 10:37:22 -0300

    Versão 1.6 (OFC)

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-// 1. Seleciona o formulário de login através da classe que você criou
 const loginForm = document.querySelector('.login-form');
 
-// 2. Fica "escutando" quando o usuário clica no botão "Entrar" (submit)
 loginForm.addEventListener('submit', function(event) {
-    // Evita que a página recarregue e limpe os campos antes da hora
     event.preventDefault();
 
-    // 3. Pega os valores digitados nos inputs usando os IDs que estão no seu HTML
     const usuarioDigitado = document.getElementById('username').value;
     const senhaDigitada = document.getElementById('password').value;
 
-    // 4. TESTE MOCK: Usuário e senha provisórios para teste
+    // Dados de teste acadêmico
     const usuarioCorreto = "vampeta";
     const senhaCorreta = "12345";
 
-    // 5. Validação simples
     if (usuarioDigitado === usuarioCorreto && senhaDigitada === senhaCorreta) {
-        alert("Login realizado com sucesso! Seja bem-vindo ao Almoxarifado.");
-        
-        // Redireciona o usuário para a página interna do sistema
-        // (Crie uma página chamada 'painel.html' depois para testar)
-        window.location.href = "painel.html"; 
+        // Alerta bonito de Sucesso
+        Swal.fire({
+            icon: 'success',
+            title: 'Acesso Autorizado!',
+            text: 'Seja bem-vindo ao Vampeta Almoxarifados.',
+            timer: 2000,
+            showConfirmButton: false
+        }).then(() => {
+            // Após 2 segundos, joga o usuário para a página de Estoque (Página 2)
+            window.location.href = "estoque.html"; 
+        });
     } else {
-        // Alerta caso erre os dados
-        alert("Usuário ou senha incorretos! Tente novamente.");
+        // Alerta bonito de Erro
+        Swal.fire({
+            icon: 'error',
+            title: 'Ops...',
+            text: 'Usuário ou senha incorretos!',
+            confirmButtonColor: '#ff9500' // O laranja do seu botão
+        });
     }
-}); //comentario pra atualizar no git 2
+});
